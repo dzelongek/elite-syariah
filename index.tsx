28ffdadbf4d60b2c0295546af2f3155ca2602d50
dzelongek <42103779+dzelongek@users.noreply.github.com>
2026-02-18 03:18:45 +0700

    font delivery

--- index.tsx
+++ index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
+import '@fontsource/lato';
+import '@fontsource/montserrat';
+import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
