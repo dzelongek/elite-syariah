4700cd96f552df3db091b58602cfd3765e613dcb
dzelongek <42103779+dzelongek@users.noreply.github.com>
2026-02-18 03:24:08 +0700

    rollback font

--- index.tsx
+++ index.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
-import '@fontsource/lato';
-import '@fontsource/montserrat';
-import './index.css';
+
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
